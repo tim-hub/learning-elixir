fc21a21b9d2ab505ab448f4a399d3bc28dec2b03
tim-hub <tim-hub@users.noreply.github.com>
2023-06-19 15:58:38 +1200

    try 1st for exercise

--- wizard/lib/chap_03/logic_exercise.ex
+++ wizard/lib/chap_03/logic_exercise.ex
@@ -18,38 +18,38 @@ defmodule Wizard.LogicExercise do
   """
 
   def run() do
-    answer_1 = nil
+    answer_1 = 1
     test_1 = is_integer(answer_1)
 
     # replace the answer variable value with a value that will make the test variable equal true
-    answer_2 = nil
+    answer_2 = 1.0
     test_2 = is_float(answer_2)
 
-    answer_3 = nil
+    answer_3 = "confusing binary in elixir"
     test_3 = is_binary(answer_3)
 
-    answer_4 = nil
+    answer_4 = [1]
     test_4 = is_list(answer_4)
 
-    answer_5 = nil
+    answer_5 = true
     test_5 = true and answer_5
 
-    answer_6 = nil
+    answer_6 = true
     test_6 = nil || answer_6
 
-    answer_7 = nil
+    answer_7 = false
     test_7 = !answer_7
 
-    answer_8 = nil
+    answer_8 = 1
     test_8 = !is_atom(answer_8)
 
-    answer_9 = nil
+    answer_9 = :true
     test_9 = false or answer_9
 
-    answer_10 = ""
+    answer_10 = "1"
     test_10 = String.length(answer_10) && true
 
-    answer_11 = %{}
+    answer_11 = %{distance: 1}
     test_11 = Map.get(answer_11, "distance") && true
 
     test_1 && test_2 && test_3 && test_4 && test_5 && test_6 && test_7 && test_8 && test_9 && test_10 && test_11
