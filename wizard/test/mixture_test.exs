bcdc5becf171d057fe099f19b756e8db64b3c050
tim-hub <tim-hub@users.noreply.github.com>
2023-06-30 15:11:26 +1200

    did mix dependency uni test practice

--- wizard/test/mixture_test.exs
+++ wizard/test/mixture_test.exs
@@ -1,5 +1,13 @@
 defmodule Wizard.MixtureTest do
-  
+  use ExUnit.Case
+  doctest Wizard
+
+  test "First ready befopre second" do
+    first = Wizard.Mixture.mix_time()
+    second = Wizard.Mixture.mix_time()
+
+    assert Timex.before?(first, second)
+  end
 
   
 end
